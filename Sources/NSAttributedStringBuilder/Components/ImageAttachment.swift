727d00d2aab73b123aeb557075a8793ea65b01cc
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2022-12-25 17:38:26 +0800

    support NSImage

--- Sources/NSAttributedStringBuilder/Components/ImageAttachment.swift
+++ Sources/NSAttributedStringBuilder/Components/ImageAttachment.swift
@@ -1,38 +1,40 @@
 #if canImport(UIKit)
-    import UIKit
+import UIKit
+public typealias Image = UIImage
+#elseif canImport(AppKit)
+import AppKit
+public typealias Image = NSImage
+#endif
+#if !os(watchOS)
+public typealias ImageAttachment = NSAttributedString.ImageAttachment
 
-    #if !os(watchOS)
-        public typealias ImageAttachment = NSAttributedString.ImageAttachment
+public extension NSAttributedString {
+    struct ImageAttachment: Component {
+        // MARK: Lifecycle
 
-        public extension NSAttributedString {
-            struct ImageAttachment: Component {
-                // MARK: Lifecycle
+        public init(_ image: Image, size: Size? = nil) {
+            let attachment = NSTextAttachment()
+            attachment.image = image
 
-                public init(_ image: UIImage, size: Size? = nil) {
-                    let attachment = NSTextAttachment()
-                    attachment.image = image
-
-                    if let size = size {
-                        attachment.bounds.size = size
-                    }
-
-                    self.attachment = attachment
-                }
-
-                // MARK: Public
-
-                public let string: String = ""
-                public let attributes: Attributes = [:]
-
-                public var attributedString: NSAttributedString {
-                    NSAttributedString(attachment: attachment)
-                }
-
-                // MARK: Private
-
-                private let attachment: NSTextAttachment
+            if let size = size {
+                attachment.bounds.size = size
             }
-        }
-    #endif
 
+            self.attachment = attachment
+        }
+
+        // MARK: Public
+
+        public let string: String = ""
+        public let attributes: Attributes = [:]
+
+        public var attributedString: NSAttributedString {
+            NSAttributedString(attachment: attachment)
+        }
+
+        // MARK: Private
+
+        private let attachment: NSTextAttachment
+    }
+}
 #endif
